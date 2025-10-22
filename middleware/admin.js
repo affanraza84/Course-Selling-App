f2f856b012f13fc0e297ae0217abf19ef04f0ed7
Affan Raza <affanraza@Affans-MacBook-Air.local>
2025-10-22 17:29:14 +0530

    updated middlwares

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,9 +1,8 @@
 const jwt = require('jsonwebtoken');
-const { JWT_ADMIN_PASSWORD } = require("../config");
 
 function adminMiddleware(req,res,next){
     const token = req.headers.token;
-    const decodedInformation = jwt.verify(token, JWT_ADMIN_PASSWORD);
+    const decodedInformation = jwt.verify(token, process.env.JWT_ADMIN_PASSWORD);
 
     if(decodedInformation){
         req.userId = decodedInformation.id;
--- middleware/user.js
+++ middleware/user.js
@@ -1,9 +1,8 @@
 const jwt = require('jsonwebtoken');
-const {JWT_USER_PASSWORD} = require('../config');
 
 function userMiddleware(req, res, next) {
     const token = req.headers.token;
-    const decodedInformation = jwt.verify(token, JWT_USER_PASSWORD);
+    const decodedInformation = jwt.verify(token, process.env.JWT_USER_PASSWORD);
 
     if(decodedInformation){
         req.userId = decodedInformation.id;
